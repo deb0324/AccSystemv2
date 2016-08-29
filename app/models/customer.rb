ef3457a242f6775affe30ac29943d5c182536af4
Debbie Tseng <debbie_wei0324@hotmail.com>
2016-08-28 19:56:09 -0700

    added model associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ActiveRecord::Base
+  has_many :tasks
+
+  belongs_to :officer, foreign_key: :officer_id, class_name: 'User'
+  belongs_to :leader, foreign_key: :leader_id, class_name: 'User'
+  belongs_to :manager, foreign_key: :manager_id, class_name: 'User'
 end
